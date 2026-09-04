2683057203d10b0de34436d6fa883b541d05c3cf
Floki <mats.hokland@outlook.com>
2026-09-04 08:01:57 +0200

    feat: add properties to WorkOrder

--- WorkOrderManagement.Api/Models/WorkOrder.cs
+++ WorkOrderManagement.Api/Models/WorkOrder.cs
@@ -2,5 +2,10 @@
 {
     public class WorkOrder
     {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string Status { get; set; } = "";
+        public string Priority { get; set; } = "";
     }
 }
