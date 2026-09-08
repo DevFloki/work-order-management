51f18020c7e657e0759b35b74b7849ff5323dd2b
Floki <mats.hokland@outlook.com>
2026-09-08 14:38:16 +0200

    feat: add postgresql database integration with appdbcontext

---
+++ WorkOrderManagement.Api/Data/AppDbContext.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkOrderManagement.Api.Models;
+
+namespace WorkOrderManagement.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<Asset> Assets => Set<Asset>();
+        public DbSet<Technician> Technicians => Set<Technician>();
+        public DbSet<WorkOrder> WorkOrders => Set<WorkOrder>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.ToTable("customers");
+
+                entity.Property(customer => customer.Id)
+                .HasColumnName("id");
+
+                entity.Property(customer => customer.Name)
+                .HasColumnName("name");
+            });
+
+            modelBuilder.Entity<Asset>(entity =>
+            {
+                entity.ToTable("assets");
+
+                entity.Property(asset => asset.Id)
+                    .HasColumnName("id");
+
+                entity.Property(asset => asset.Name)
+                    .HasColumnName("name");
+
+                entity.Property(asset => asset.CustomerId)
+                    .HasColumnName("customer_id");
+            });
+
+            modelBuilder.Entity<Technician>(entity =>
+            {
+                entity.ToTable("technicians");
+
+                entity.Property(technician => technician.Id)
+                    .HasColumnName("id");
+
+                entity.Property(technician => technician.Name)
+                    .HasColumnName("name");
+            });
+
+            modelBuilder.Entity<WorkOrder>(entity =>
+            {
+                entity.ToTable("work_orders");
+
+                entity.Property(workOrder => workOrder.Id)
+                    .HasColumnName("id");
+
+                entity.Property(workOrder => workOrder.Title)
+                    .HasColumnName("title");
+
+                entity.Property(workOrder => workOrder.Description)
+                    .HasColumnName("description");
+
+                entity.Property(workOrder => workOrder.Status)
+                    .HasColumnName("status");
+
+                entity.Property(workOrder => workOrder.Priority)
+                    .HasColumnName("priority");
+
+                entity.Property(workOrder => workOrder.AssetId)
+                    .HasColumnName("asset_id");
+
+                entity.Property(workOrder => workOrder.TechnicianId)
+                    .HasColumnName("technician_id");
+            });
+        }
+    }
+}
--- WorkOrderManagement.Api/Program.cs
+++ WorkOrderManagement.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using WorkOrderManagement.Api.Data;
 using WorkOrderManagement.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +9,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
 builder.Services.AddScoped<IWorkOrderService, WorkOrderService>();
+
+var connectionString = builder.Configuration.GetConnectionString(
+    "DefaultConnection") ?? throw new InvalidOperationException(
+        "Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
